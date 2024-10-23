77d0b3de5a6926693c02358e1e1689738a37cf46
dineshmutcheti <140743420+dineshmutcheti@users.noreply.github.com>
2024-10-23 09:56:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/transactions', transactionRoutes);
-app.use('/', categoryRoutes); // Categories are accessible without a prefix
+app.use('/', categoryRoutes); 
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
